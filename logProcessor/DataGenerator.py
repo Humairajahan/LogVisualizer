d447e519ecdca68daaacedc4919035baa518c206
Humairajahan <humairaazim45@gmail.com>
2023-06-21 12:56:57 +0600

    added sleeping for random intervals for data generation

--- logProcessor/DataGenerator.py
+++ logProcessor/DataGenerator.py
@@ -1,18 +1,24 @@
 import asyncio
-
+import random
 import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
-file_handler = logging.FileHandler('./LogFile.log')
+formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
+file_handler = logging.FileHandler("./LogFile.log")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+
+
+def asleep_for_time():
+    return random.randrange(9)
+
 
 async def createEntry():
     while True:
         logger.info("Entry created")
-        await asyncio.sleep(4)
+        await asyncio.sleep(asleep_for_time())
+
 
 if __name__ == "__main__":
     asyncio.run(createEntry())
