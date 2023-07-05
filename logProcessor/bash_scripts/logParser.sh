5d2c143134581b81166e7f92721753962ac61fc4
Humairajahan <humairaazim45@gmail.com>
2023-07-05 15:34:22 +0600

    added apache kafka producer

--- logProcessor/bash_scripts/logParser.sh
+++ logProcessor/bash_scripts/logParser.sh
@@ -2,6 +2,10 @@
 
 logfile="./LogFile.log"
 
+KAFKA_BROKER="PLAINTEXT://kafka:29092"
+KAFKA_TOPIC="MSGS"
+
 tail -n 0 -F "$logfile" | while read -r line; do
+    kafkacat -b "$KAFKA_BROKER" -t "$KAFKA_TOPIC" -P <<< "$line"
     echo "$line"
 done
